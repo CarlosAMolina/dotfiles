e4515a73020d883f80397138665d9f9c7e57959b
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-02-09 00:34:40 +0100

    st: add font size, color and zoom

--- dotfiles/config/st/config.h
+++ dotfiles/config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -120,7 +120,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"gray90", /* default foreground colour */
+	"#aaaaaa", /* default foreground colour */
 	"black", /* default background colour */
 };
 
@@ -203,6 +203,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ControlMask,          XK_j,           zoom,           {.f = +1} },
+	{ ControlMask,          XK_k,           zoom,           {.f = -1} },
 };
 
 /*
